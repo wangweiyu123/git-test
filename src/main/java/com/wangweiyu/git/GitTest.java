9991eb1307b94a523feff1a2bc57f2ca7a749365
wangweiyu123 <112057500+wangweiyu123@users.noreply.github.com>
2023-03-22 16:49:55 +0800

    pull commit

--- src/main/java/com/wangweiyu/git/GitTest.java
+++ src/main/java/com/wangweiyu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test！");
         System.out.println("hox-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
