e74aa3478e803fed9ccff11742dd3db421479ec2
wangweiyu <2519745298@qq.com>
2023-03-22 15:22:08 +0800

    second commit

--- src/main/java/com/wangweiyu/git/GitTest.java
+++ src/main/java/com/wangweiyu/git/GitTest.java
@@ -2,6 +2,7 @@ package com.wangweiyu.git;
 
 public class GitTest {
     public static void main(String[] args){
-        System.out.println("hello git");
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
